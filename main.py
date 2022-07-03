bdba7cff77074835214a54908fca2ed262478e20
John Mihal <107288799+johnmihal@users.noreply.github.com>
2022-07-03 15:01:42 -0700

    added test answer key and grading
    the main thing i did was make it so that the inputs are transferred to a 2d array where I can then grade them against a key. This also involved takes the brightness of each "bubble crop".

--- main.py
+++ main.py
@@ -1,26 +1,81 @@
-from PIL import Image
+from PIL import Image, ImageStat
 import os
 
+# crops answer bullets
 def imgcrop(input, xPieces, yPieces):
     filename, file_extension = os.path.splitext(input[input.find('/'):])
     im = Image.open(input)
     imgwidth, imgheight = im.size
     height = imgheight // yPieces
     width = imgwidth // xPieces
-    for i in range(0, yPieces):
-        for j in range(0, xPieces):
+
+    #   output array
+    rows, cols = (yPieces,xPieces)
+    arr = [[0 for i in range(cols)] for j in range(rows)]
+
+    for i in range(0, rows):
+        for j in range(0, cols):
             box = (j * width, i * height, (j + 1) * width, (i + 1) * height)
             a = im.crop(box)
             try:
-                a.save("Images/Split/" + filename + "-" + str(i) + "-" + str(j) + file_extension)
+                #a.save("Images/Split/" + filename + "-" + str(i) + "-" + str(j) + file_extension)
+                lumosity = brightness(a)
+
+                if lumosity > 117:
+                    arr[i][j] = 0 # 0 = blank
+                else:
+                    arr[i][j] = 1 # 1 = filled
             except Exception as err:
                 print(err)
                 pass
+    return arr
+
+#   Returns Greyscale Brightness 255 = white, 0 = black
+def brightness(a):
+   im = a.convert('L')
+   stat = ImageStat.Stat(im)
+   return stat.mean[0]
+
+#   coordinates the data collection from the input image
+def getInput(img, col, row):
+    answers = imgcrop("Images/gridTest1.png", col, row)
+    return answers
+
+# Loads the test answer key
+def loadAnswerKey(col, row):
+    key = [[0 for i in range(col)] for j in range(row)]
+
+    for j in range(0, row):
+        key[j][0] = 1
+    return key
+
+# grades the test
+def grade(key, input, col, row):
+    for i in range(0, row):
+        correct = True
+        for j in range(0, col):
+            if key[i][j] != input[i][j]:
+                correct = False
+        if correct:
+            print(i+1, " Correct")
+        else:
+            print(i+1, " Incorrect")
+
 
 def main():
-    print("hello")
-    print(os.getcwd())
-    imgcrop("Images/gridTest1.png",4,8)
+    print(os.getcwd())  # this tells you what directory you are running from, good for debugging.
+
+    # These indicate the number of questions as row and the number of
+    # potential answers per question is col
+    col = 4
+    row = 8
+
+    key = loadAnswerKey(col, row)
+    print(key)
+    input = getInput("Images/gridTest1.png", col, row)
+    print(input)
+    grade(key, input, col, row)
+
 
 if __name__ == "__main__":
     main()
